1902d580b854520a5d03e648446eed0fa86e7563
Unknown <vincent1906@live.nl>
2018-12-05 13:29:00 +0100

    Started work on a page for adding games to collection

--- app/src/main/java/autiboiz/collectiontestapp/Add.java
+++ app/src/main/java/autiboiz/collectiontestapp/Add.java
@@ -1,4 +1,13 @@
 package autiboiz.collectiontestapp;
 
-public class Add {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Add extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addpage);
+    }
 }
--- app/src/main/java/autiboiz/collectiontestapp/MainPage.java
+++ app/src/main/java/autiboiz/collectiontestapp/MainPage.java
@@ -1,5 +1,6 @@
 package autiboiz.collectiontestapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,6 +10,11 @@ public class MainPage extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+    }
+
+    public void addPage(){
+        Intent intent = new Intent(this, Add.class);
+        startActivity(intent);
     }
 
 }
